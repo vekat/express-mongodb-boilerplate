758a61d4ac788ac2bb9d3fdcc3737711eacfb507
Italo Brito Brasil <italobritoandroid@gmail.com>
2016-02-29 20:17:00 -0300

    added required params

--- schemas/beer.js
+++ schemas/beer.js
@@ -3,9 +3,9 @@ var mongoose    = require('mongoose');
 
 //create BeerSchema as a Mongoose Schema
 var BeerSchema  = new mongoose.Schema({
-  name: String,
-  type: String,
-  quantity: Number
+  name: { type: String, required: true },
+  type: { type: String, required: true },
+  quantity: { type: Number, default: 0 }
 });
 
 //export as default
