4fb1c9dd16c6da3c86e3d7e530eef6be4ae4844f
Vitor Cortez <vitor.a.cortez@gmail.com>
2016-03-03 21:33:51 -0300

    Remove `method-override`, add comments to index

--- README.md
+++ README.md
@@ -5,7 +5,6 @@ Node.js application boilerplate based on [Express.js](http://expressjs.com/) and
 * [bluebird](https://www.npmjs.com/package/bluebird) Full featured Promises/A+ implementation with exceptionally good performance;
 * [body-parser](https://www.npmjs.com/package/body-parser) Node.js body parsing middleware;
 * [express](https://www.npmjs.com/package/express) Fast, unopinionated, minimalist web framework;
-* [method-override](https://www.npmjs.com/package/method-override) Override HTTP verbs;
 * [mongoose](https://www.npmjs.com/package/mongoose) Mongoose MongoDB ODM;
 * [morgan](https://www.npmjs.com/package/morgan) HTTP request logger middleware for node.js;
 
--- index.js
+++ index.js
@@ -1,36 +1,45 @@
-var fs = require("fs"),
-http = require("http"),
-express = require("express"),
-mongoose = require("mongoose"),
-app = express(),
-path = require('path'),
-methodOverride = require('method-override'),
-logger = require('morgan'),
-bodyParser = require('body-parser');
-mongoose.Promise = require('bluebird');
-db = mongoose.connection;
+var fs = require('fs'),
+  http = require('http'),
+  express = require('express'),
+  mongoose = require('mongoose'),
+  logger = require('morgan'),
+  bodyParser = require('body-parser');
 
-app.use(logger('dev'));
-app.use(methodOverride());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.set('view engine', 'express');
+mongoose.Promise = require('bluebird');
+
+// obtendo conexao com o banco e capturando eventos
+var db = mongoose.connection;
 
 db.on('error', console.error);
+
 db.once('open', function() {
-    console.log('Conexão estabelecida...');
+  console.log('Conexão estabelecida...');
 });
-mongoose.connect('mongodb://127.0.0.1/express-mongodb-boilerplate');
-fs.readdirSync(__dirname + '/schemas/').forEach(function (file) {
-	if (~file.indexOf('.js')) require(__dirname + '/schemas/' + file);
-});
+
+// ponto inicial de aplicacoes express: http://goo.gl/j4K9yt
+var app = express();
+
+// montagem de middlewares
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use('/', require('./routes'));
-app.use(function(err, req, res, next){
-    if (res.headersSent) return next(err);
-    res.status(500).json({ message : err.message });
+
+// middlewares de tratamento de erro
+app.use(function(req, res, next) {
+  res.status(404).json({ message: 'Página não encontrada' });
 });
-app.use(function(req, res, next){
-	res.status(404).json({ message: 'Página não encontrada' });
+
+app.use(function(err, req, res, next) {
+  if (res.headersSent) return next(err);
+  res.status(500).json({ message: err.message });
 });
+
+// conexao com o banco
+mongoose.connect('mongodb://127.0.0.1/express-mongodb-boilerplate');
+
+// iniciando servidor
 http.createServer(app).listen(3040);
-console.log("Server starter on :3040");
+
+console.log('Servidor ouvindo em http://localhost:3040');
